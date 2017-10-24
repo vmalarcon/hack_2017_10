d2b29c8852c10d37452ac0e3a624d9db623aa996
Victor Alarcon <valarcon@expedia.com>
2017-10-24 11:36:35 -0400

    initial commit

---
+++ src/detect_bw.py
@@ -0,0 +1,45 @@
+# import the necessary packages
+from imutils import paths
+import argparse
+import cv2
+from PIL import Image, ImageStat
+
+def detect_color_image(file, thumb_size=40, MSE_cutoff=22, adjust_color_bias=True):
+    pil_img = Image.open(file)
+    bands = pil_img.getbands()
+    if bands == ('R','G','B') or bands== ('R','G','B','A'):
+        thumb = pil_img.resize((thumb_size,thumb_size))
+        SSE, bias = 0, [0,0,0]
+        if adjust_color_bias:
+            bias = ImageStat.Stat(thumb).mean[:3]
+            bias = [b - sum(bias)/3 for b in bias ]
+        for pixel in thumb.getdata():
+            mu = sum(pixel)/3
+            SSE += sum((pixel[i] - mu - bias[i])*(pixel[i] - mu - bias[i]) for i in [0,1,2])
+        MSE = float(SSE)/(thumb_size*thumb_size)
+        if MSE <= MSE_cutoff:
+            print "grayscale\t",
+        else:
+            print "Color\t\t\t",
+        print "( MSE=",MSE,")"
+    elif len(bands)==1:
+        print "Black and white", bands
+    else:
+        print "Don't know...", bands
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True,
+	help="path to input directory of images")
+ap.add_argument("-t", "--threshold", type=float, default=100.0,
+	help="focus measures that fall below this value will be considered 'blurry'")
+args = vars(ap.parse_args())
+
+# loop over the input images
+for imagePath in paths.list_images(args["images"]):
+	detect_color_image(imagePath)	
+
+	image = cv2.imread(imagePath) 
+	cv2.imshow("Image", image)
+	key = cv2.waitKey(0)
+
---
+++ src/detect_text.py
@@ -0,0 +1,68 @@
+# import the necessary packages
+from imutils import paths
+import argparse
+import cv2
+from PIL import Image
+import pytesseract
+
+def captch_ex(file_name):
+    img = cv2.imread(file_name)
+
+    img_final = cv2.imread(file_name)
+    img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 180, 255, cv2.THRESH_BINARY)
+    image_final = cv2.bitwise_and(img2gray, img2gray, mask=mask)
+    ret, new_img = cv2.threshold(image_final, 180, 255, cv2.THRESH_BINARY)  # for black text , cv.THRESH_BINARY_INV
+    '''
+            line  8 to 12  : Remove noisy portion 
+    '''
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,
+                                                         3))  # to manipulate the orientation of dilution , large x means horizonatally dilating  more, large y means vertically dilating more
+    dilated = cv2.dilate(new_img, kernel, iterations=9)  # dilate , more the iteration more the dilation
+
+    # for cv2.x.x
+    #contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # get contours
+
+    # for cv3.x.x comment above line and uncomment line below
+    image, contours, hierarchy = cv2.findContours(dilated,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        # get rectangle bounding contour
+        [x, y, w, h] = cv2.boundingRect(contour)
+
+        # Don't plot small false positives that aren't text
+        if w < 35 and h < 35:
+            continue
+
+        # draw rectangle around contour on original image
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+
+        # you can crop image and send to OCR  , false detected will return no text :)
+        cropped = img_final[y :y +  h , x : x + w]
+
+	detected_text =  pytesseract.image_to_string(Image.fromarray(cropped))
+	'''
+        s = file_name + '/crop_' + str(index) + '.jpg' 
+        cv2.imwrite(s , cropped)
+        index = index + 1
+
+        '''
+    	# write original image with added contours to disk
+	if len(detected_text) >= 1:
+		print detected_text
+    		cv2.imshow('captcha_result', img)
+    		cv2.waitKey()
+
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True,
+	help="path to input directory of images")
+ap.add_argument("-t", "--threshold", type=float, default=100.0,
+	help="focus measures that fall below this value will be considered 'blurry'")
+args = vars(ap.parse_args())
+
+# loop over the input images
+for imagePath in paths.list_images(args["images"]):
+	captch_ex(imagePath)
+
